9557408ca89022270d28dcfa32b0d2171521f4e9
Agustin <isasmendi.agus@gmail.com>
2022-06-24 10:26:20 +0100

    Keep version parsed on empty response

--- src/bin/cli-bin.ts
+++ src/bin/cli-bin.ts
@@ -16,7 +16,7 @@ function CLIErrorHandler(e: Error) {
 
 async function main() {
   program
-    .version("0.2.28")
+    .version("0.2.3")
     .description('The SCANOSS JS package provides a simple, easy to consume module for interacting with SCANOSS APIs/Engine.')
 
   program
