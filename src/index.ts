21c1a59ade448fe7092cbddd4e6177d8e63fb7bb
Agustin <isasmendi.agus@gmail.com>
2022-10-12 14:09:22 +0200

    export unzip module and changed version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
+// *** Code scanner exports *** //
+export * from './sdk/scanner/Scanner';
 export * from './sdk/scanner/ScannerTypes';
-export * from './sdk/scanner/ScannerCfg'
-export * from './sdk/scanner/Scanner'
+export * from './sdk/scanner/ScannerCfg';
 export * from './sdk/scanner/WfpProvider/WfpCalculator/WfpCalculator';
+
+// *** Dependency scanner exports *** //
 export * from './sdk/Dependencies/DependencyTypes';
 export * from './sdk/Dependencies/DependencyScannerCfg';
 export * from './sdk/Dependencies/DependencyScanner';
 
 
+// *** Data layers export *** //
+export * from './sdk/DataLayer/DataLayerTypes';
+export * from './sdk/DataLayer/DataProviderManager';
+export * from './sdk/DataLayer/DataProviders/LicenseDataProvider';
+export * from './sdk/DataLayer/DataProviders/SummaryDataProvider';
+export * from './sdk/DataLayer/DataProviders/DependencyDataProvider';
+export * from './sdk/DataLayer/DataProviders/ComponentDataProvider';
 
+// *** Unzip *** //
+export * from './sdk/Decompress/DecompressionManager';
