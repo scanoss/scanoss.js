f10bcbb82be5c935777d357e799777044994e2d9
Agustin <isasmendi.agus@gmail.com>
2022-11-18 16:35:19 +0100

    Updated version

--- src/cli/bin/cli-bin.ts
+++ src/cli/bin/cli-bin.ts
@@ -16,7 +16,7 @@ function CLIErrorHandler(e: Error) {
 
 async function main() {
   program
-    .version("0.4.13-beta")
+    .version("0.5.0")
     .description('The SCANOSS JS package provides a simple, easy to consume module for interacting with SCANOSS APIs/Engine.')
 
   program
