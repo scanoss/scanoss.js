4c1b5f68cf68afe2cc863728b5a4c4108ca1ed1f
Agustin Isasmendi <isasmendi.agus@gmail.com>
2023-02-02 17:15:41 +0100

    CE-1409 Exposes dependency filter function

--- tests/sdk/Dependencies/LocalDependency/LocalDependency.spec.ts
+++ tests/sdk/Dependencies/LocalDependency/LocalDependency.spec.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import path from 'path';
 import { LocalDependencies } from  '../../../../src/sdk/Dependencies/LocalDependency/LocalDependency'
 
@@ -7,15 +7,38 @@ describe('Suit test for LocalDependency Scanner', () => {
 
   it('Testing wildcard string matching',  function () {
     const localDependencyScanner = new LocalDependencies();
-
     assert(localDependencyScanner.stringMatchWithWildcard("hello this is a test", "hello*"));
     assert(localDependencyScanner.stringMatchWithWildcard("hello this is a test", "*hello*"));
     assert(localDependencyScanner.stringMatchWithWildcard("hello this is a test", "*"));
     assert(localDependencyScanner.stringMatchWithWildcard("hello this is a test", "*this*"));
     assert(localDependencyScanner.stringMatchWithWildcard("/home/user/projname.cproj", "*.cproj"));
-
     assert(!localDependencyScanner.stringMatchWithWildcard("this text should no match", "*hello*"));
     assert(!localDependencyScanner.stringMatchWithWildcard("/home/user/projname.cproj", "*hello"));
+  });
+
+
+
+  it('Testing filepath filter function',  function () {
+    const localDependencyScanner = new LocalDependencies();
+
+    const files = [
+      "/home/user/ignore.c",
+      "/home/user/go.sum",
+      "/home/user/accept.csproj",
+      "/home/user/ignore2.c",
+    ];
+
+    const expectedOut = [
+      "/home/user/go.sum",
+      "/home/user/accept.csproj",
+    ];
+
+    const result = localDependencyScanner.filterFiles(files);
+
+    for (let i=0; i<result.length;i++){
+      expect(expectedOut[i]).to.be.equal(result[i]);
+    }
+
 
   });
 
