273b14b341e27bc80063eb622db15c8332836a7b
Agustin Isasmendi <agustin.isasmendi@scanoss.com>
2023-04-16 18:51:01 +0200

    CLIS-77 Fix SDK on Vs Code environment
    CLIS-77 Fix SDK on Vs Code environment

--- src/sdk/Utils/Utils.ts
+++ src/sdk/Utils/Utils.ts
@@ -8,11 +8,42 @@ import * as Http from 'http';
 import * as Https from 'https';
 import { Logger, logger } from '../Logger';
 import * as isInNet2 from 'pac-resolver/dist/isInNet';
+import fs from 'fs';
+import tls from 'tls';
 
 
 export class Utils {
   private static PackageJSON: any = null;
   private static PAC_FindProxyForURL: FindProxyForURL;
+
+  // This function takes inspiration from https://www.npmjs.com/package/syswide-cas
+  // Copyright 2016 Capriza. Code released under the MIT license
+  public static loadCaCertFromFile(file: string) {
+    const rootCAs = [];
+
+    let content = fs.readFileSync(file, { encoding: "utf-8" }).trim();
+    content = content.replace(/\r\n/g, "\n"); // Handles certificates that have been created in Windows
+    const regex = /-----BEGIN CERTIFICATE-----\n[\s\S]+?\n-----END CERTIFICATE-----/g;
+    const results = content.match(regex);
+    if (!results) throw new Error("Could not parse certificate");
+
+    results.forEach((match)=> {
+      const cert = match.trim();
+      rootCAs.push(cert);
+    });
+
+    const origCreateSecureContext = tls.createSecureContext;
+    tls.createSecureContext = function(options) {
+      var c = origCreateSecureContext.apply(null, arguments);
+      if (!options.ca && rootCAs.length > 0) {
+        rootCAs.forEach(function(ca) {
+          // add to the created context our own root CAs
+          c.context.addCACert(ca);
+        });
+      }
+      return c;
+    };
+  }
 
   public static getPackageVersion(): string {
     if (!this.PackageJSON) {
@@ -29,7 +60,6 @@ export class Utils {
       }
     }
     return this.PackageJSON?.version ? this.PackageJSON.version : ''
-
   }
 
 
