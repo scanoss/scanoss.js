973490e83913f85ba19a79289822f72c602558aa
Agustin Isasmendi <isasmendi.agus@gmail.com>
2023-12-19 12:53:21 +0100

    CLIS-85 Fixes dump data into bad_request file

--- src/sdk/scanner/Dispatcher/Dispatcher.ts
+++ src/sdk/scanner/Dispatcher/Dispatcher.ts
@@ -2,12 +2,12 @@
 /* eslint-disable no-restricted-syntax */
 import EventEmitter from 'eventemitter3';
 import fetch, { Response } from 'node-fetch';
-import PQueue from "p-queue";
+import PQueue from 'p-queue';
 
-import { ScannerEvents } from "../ScannerTypes";
-import { DispatcherResponse } from "./DispatcherResponse";
-import { ScannerCfg } from "../ScannerCfg";
-import { GlobalControllerAborter } from "./GlobalControllerAborter";
+import { ScannerEvents } from '../ScannerTypes';
+import { DispatcherResponse } from './DispatcherResponse';
+import { ScannerCfg } from '../ScannerCfg';
+import { GlobalControllerAborter } from './GlobalControllerAborter';
 import { DispatchableItem } from './DispatchableItem';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 import { HttpProxyAgent } from 'http-proxy-agent';
@@ -35,26 +35,34 @@ export class Dispatcher extends EventEmitter {
   constructor(scannerCfg = new ScannerCfg()) {
     super();
     this.scannerCfg = scannerCfg;
-    if(this.scannerCfg.CONCURRENCY_LIMIT > MAX_CONCURRENT_REQUEST)
+    if (this.scannerCfg.CONCURRENCY_LIMIT > MAX_CONCURRENT_REQUEST)
       this.scannerCfg.CONCURRENCY_LIMIT = MAX_CONCURRENT_REQUEST;
 
     this.init();
   }
 
   init() {
-
     //Loads proxy from SDK config, if not, loads from env variables, if not, leave empty
     this.proxyAgent = null;
     this.caCert = null;
 
-    const proxyAddr = this.scannerCfg.PROXY || process.env.https_proxy || process.env.HTTPS_PROXY || process.env.http_proxy  || process.env.HTTP_PROXY || null;
-    const caCertPath = this.scannerCfg.CA_CERT  || process.env.NODE_EXTRA_CA_CERTS
+    const proxyAddr =
+      this.scannerCfg.PROXY ||
+      process.env.https_proxy ||
+      process.env.HTTPS_PROXY ||
+      process.env.http_proxy ||
+      process.env.HTTP_PROXY ||
+      null;
+    const caCertPath =
+      this.scannerCfg.CA_CERT || process.env.NODE_EXTRA_CA_CERTS;
 
     if (caCertPath) Utils.loadCaCertFromFile(caCertPath);
-    else if (this.scannerCfg.IGNORE_CERT_ERRORS || proxyAddr) process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    else if (this.scannerCfg.IGNORE_CERT_ERRORS || proxyAddr)
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
     if (proxyAddr) {
-      if (this.scannerCfg.API_URL.trim().startsWith('https')) this.proxyAgent = new HttpsProxyAgent(proxyAddr);
+      if (this.scannerCfg.API_URL.trim().startsWith('https'))
+        this.proxyAgent = new HttpsProxyAgent(proxyAddr);
       else this.proxyAgent = new HttpProxyAgent(proxyAddr);
     }
 
@@ -68,7 +76,11 @@ export class Dispatcher extends EventEmitter {
     });
 
     this.pQueue.on('next', () => {
-      if ((this.pQueue.size + this.pQueue.pending) < this.scannerCfg.DISPATCHER_QUEUE_SIZE_MIN_LIMIT && !this.queueMinLimitReached) {
+      if (
+        this.pQueue.size + this.pQueue.pending <
+          this.scannerCfg.DISPATCHER_QUEUE_SIZE_MIN_LIMIT &&
+        !this.queueMinLimitReached
+      ) {
         this.emit(ScannerEvents.DISPATCHER_QUEUE_SIZE_MIN_LIMIT);
         this.queueMinLimitReached = true;
         this.queueMaxLimitReached = false;
@@ -95,7 +107,8 @@ export class Dispatcher extends EventEmitter {
     this.pQueue.add(() => this.dispatch(item));
 
     if (
-      this.pQueue.size + this.pQueue.pending >= this.scannerCfg.DISPATCHER_QUEUE_SIZE_MAX_LIMIT &&
+      this.pQueue.size + this.pQueue.pending >=
+        this.scannerCfg.DISPATCHER_QUEUE_SIZE_MAX_LIMIT &&
       !this.queueMaxLimitReached
     ) {
       this.emit(ScannerEvents.DISPATCHER_QUEUE_SIZE_MAX_LIMIT);
@@ -109,26 +122,35 @@ export class Dispatcher extends EventEmitter {
   }
 
   emitNoDispatchedItem(disptItem) {
-    this.emit(ScannerEvents.DISPATCHER_LOG, `[ SCANNER ]: WFP content sended to many times. Some files won't be scanned`);
+    this.emit(
+      ScannerEvents.DISPATCHER_LOG,
+      `[ SCANNER ]: WFP content sended to many times. Some files won't be scanned`
+    );
     this.emit(ScannerEvents.DISPATCHER_ITEM_NO_DISPATCHED, disptItem);
   }
 
   errorHandler(error: Error, disptItem: DispatchableItem, response: string) {
     if (!this.globalAbortController.isAborting()) {
-
       if (error.name === 'AbortError') {
-        error.message = `Timeout reached for packet with request ID ${disptItem.uuid}. Enqueuing again.`
+        error.message = `Timeout reached for packet with request ID ${disptItem.uuid}. Enqueuing again.`;
         error.name = 'TIMEOUT';
       }
 
       if (this.recoverableErrors.has(error.name)) {
         disptItem.increaseErrorCounter();
-        if (disptItem.getErrorCounter() >= this.scannerCfg.MAX_RETRIES_FOR_RECOVERABLES_ERRORS) {
+        if (
+          disptItem.getErrorCounter() >=
+          this.scannerCfg.MAX_RETRIES_FOR_RECOVERABLES_ERRORS
+        ) {
           this.emitNoDispatchedItem(disptItem);
-          if (this.scannerCfg.ABORT_ON_MAX_RETRIES) this.emitUnrecoberableError(error, disptItem, response);
+          if (this.scannerCfg.ABORT_ON_MAX_RETRIES)
+            this.emitUnrecoberableError(error, disptItem, response);
           return;
         }
-        this.emit(ScannerEvents.DISPATCHER_LOG,`[ SCANNER ]: Recoverable error happened sending WFP content to server. Reason: ${error}`);
+        this.emit(
+          ScannerEvents.DISPATCHER_LOG,
+          `[ SCANNER ]: Recoverable error happened sending WFP content to server. Reason: ${error}`
+        );
         this.dispatchItem(disptItem);
         return;
       }
@@ -138,7 +160,10 @@ export class Dispatcher extends EventEmitter {
 
   async dispatch(item: DispatchableItem) {
     const timeoutController = this.globalAbortController.getAbortController();
-    const timeoutId = setTimeout(() => timeoutController.abort(), this.scannerCfg.TIMEOUT);
+    const timeoutId = setTimeout(
+      () => timeoutController.abort(),
+      this.scannerCfg.TIMEOUT
+    );
     let plain_response: string;
     try {
       this.emit(ScannerEvents.DISPATCHER_WFP_SENDED);
@@ -146,9 +171,12 @@ export class Dispatcher extends EventEmitter {
         agent: this.proxyAgent,
         method: 'post',
         body: item.getForm(),
-        headers: {  'User-Agent': this.scannerCfg.CLIENT_TIMESTAMP ? this.scannerCfg.CLIENT_TIMESTAMP : `scanoss-js/v${Utils.getPackageVersion()}`,
-                    'X-Session': this.scannerCfg.API_KEY,
-                    'x-request-id': item.uuid,
+        headers: {
+          'User-Agent': this.scannerCfg.CLIENT_TIMESTAMP
+            ? this.scannerCfg.CLIENT_TIMESTAMP
+            : `scanoss-js/v${Utils.getPackageVersion()}`,
+          'X-Session': this.scannerCfg.API_KEY,
+          'x-request-id': item.uuid,
         },
         signal: timeoutController.signal,
       });
@@ -158,22 +186,27 @@ export class Dispatcher extends EventEmitter {
 
       if (!response.ok) {
         plain_response = await response.text();
-        const err = new Error(`\nHTTP Status code: ${response.status}\nServer Response:\n${plain_response}\n`);
+        const err = new Error(
+          `\nHTTP Status code: ${response.status}\nServer Response:\n${plain_response}\n`
+        );
         err.name = 'HTTP_ERROR';
         throw err;
       }
 
-      const dataAsText = await response.text();
-      const dataAsObj = JSON.parse(dataAsText);
+      plain_response = await response.text();
+      const dataAsObj = JSON.parse(plain_response);
 
-      const dispatcherResponse = new DispatcherResponse(dataAsObj, item.getFingerprintPackage().getContent());
+      const dispatcherResponse = new DispatcherResponse(
+        dataAsObj,
+        item.getFingerprintPackage().getContent()
+      );
       this.emit(ScannerEvents.DISPATCHER_NEW_DATA, dispatcherResponse);
       return Promise.resolve();
     } catch (e) {
-        clearTimeout(timeoutId);
-        this.globalAbortController.removeAbortController(timeoutController);
-        this.errorHandler(e, item, plain_response);
-        return Promise.resolve();
+      clearTimeout(timeoutId);
+      this.globalAbortController.removeAbortController(timeoutController);
+      this.errorHandler(e, item, plain_response);
+      return Promise.resolve();
     }
   }
 }
