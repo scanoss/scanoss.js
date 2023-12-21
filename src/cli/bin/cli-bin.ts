b0b884841fe33e5f59aa379f67731f9d65976a1f
Agustin Isasmendi <isasmendi.agus@gmail.com>
2023-12-21 16:32:10 +0100

    CLIS-36 Limit the numbers of file= in a fingerprint block

--- src/cli/bin/cli-bin.ts
+++ src/cli/bin/cli-bin.ts
@@ -6,8 +6,6 @@ import { wfpHandler } from '../commands/wfp';
 import { scanHandler } from '../commands/scan';
 import { Utils } from '../../sdk/Utils/Utils';
 
-
-
 function CLIErrorHandler(e: Error) {
   console.error(' ');
   console.error(e);
@@ -17,60 +15,135 @@ function CLIErrorHandler(e: Error) {
 async function main() {
   program
     .version(Utils.getPackageVersion())
-    .description('The SCANOSS JS package provides a simple, easy to consume module for interacting with SCANOSS APIs/Engine.')
+    .description(
+      'The SCANOSS JS package provides a simple, easy to consume module for interacting with SCANOSS APIs/Engine.'
+    );
 
   program
     .command('scan <source>')
     .description('Scan a folder/file')
     .option('-w, --wfp', 'Scan a .wfp file instead of a folder')
     .option('-H, --hpsm', 'Scan using winnowing high precision matching')
-    .option('-x, --extract', 'Extract compressed files before launch scan in folder <<zip_name>>-unzipped')
-    .option('   --extract-overwrite', 'Overwrite folder when decompressing if exists')
+    .option(
+      '-x, --extract',
+      'Extract compressed files before launch scan in folder <<zip_name>>-unzipped'
+    )
+    .option(
+      '   --extract-overwrite',
+      'Overwrite folder when decompressing if exists'
+    )
     .option('   --extract-deep <number>', 'Sets uncompress recursion level')
     .option('   --extract-suffix <suffix>', 'Sets suffix for the folder name')
-    .option('-c, --concurrency <number>', 'Number of concurrent connections to use while scanning (optional -default 10)')
-    .option('-n, --ignore <ignore>',  'Ignore components specified in the SBOM file')
-    .option('-o, --output <filename>', 'Output result file name (optional - default stdout)')
-    .option('-f, --format <format>', 'Result output format. {JSON, HTML} Default: JSON')
-    .option('-F, --flags <flags>', 'Scanning engine flags (1: disable snippet matching, 2 enable snippet ids, 4: disable dependencies, 8: disable licenses, 16: disable copyrights,32: disable vulnerabilities, 64: disable quality, 128: disable cryptography,256: disable best match, 512: Report identified files)')
-    .option('-P, --post-size <postsize>', 'Number of kilobytes to limit the post to while scanning (optional - default 64)')
-    .option('-R, --max-retry <retry>', 'Max number of retries for each POST (optional -default 5)')
-    .option('-M, --timeout <timeout>', 'Timeout (in seconds) for API communication (optional -default 120)')
+    .option(
+      '-c, --concurrency <number>',
+      'Number of concurrent connections to use while scanning (optional -default 10)'
+    )
+    .option(
+      '-n, --ignore <ignore>',
+      'Ignore components specified in the SBOM file'
+    )
+    .option(
+      '-o, --output <filename>',
+      'Output result file name (optional - default stdout)'
+    )
+    .option(
+      '-f, --format <format>',
+      'Result output format. {JSON, HTML} Default: JSON'
+    )
+    .option(
+      '-F, --flags <flags>',
+      'Scanning engine flags (1: disable snippet matching, 2 enable snippet ids, 4: disable dependencies, 8: disable licenses, 16: disable copyrights,32: disable vulnerabilities, 64: disable quality, 128: disable cryptography,256: disable best match, 512: Report identified files)'
+    )
+    .option(
+      '-P, --post-size <postsize>',
+      'Number of kilobytes to limit the post to while scanning (optional - default 32)'
+    )
+    .option(
+      '-R, --max-retry <retry>',
+      'Max number of retries for each POST (optional -default 5)'
+    )
+    .option(
+      '-M, --timeout <timeout>',
+      'Timeout (in seconds) for API communication (optional -default 120)'
+    )
     .option('--obfuscate', 'Obfuscate fingerprints')
     .option('-D, --dependencies', 'Add dependency scanning')
-    .option('--apiurl <apiurl>', 'SCANOSS API URL (optional - default: https://osskb.org/api/scan/direct)')
-    .option('--api2url <api2url>', 'SCANOSS gRPC API 2.0 URL (optional - default: scanoss.com:443)')
-    .option('-k, --key <key>', 'SCANOSS API Key token (optional - not required for default OSSKB URL)')
+    .option(
+      '--apiurl <apiurl>',
+      'SCANOSS API URL (optional - default: https://osskb.org/api/scan/direct)'
+    )
+    .option(
+      '--api2url <api2url>',
+      'SCANOSS gRPC API 2.0 URL (optional - default: scanoss.com:443)'
+    )
+    .option(
+      '-k, --key <key>',
+      'SCANOSS API Key token (optional - not required for default OSSKB URL)'
+    )
     .option('--ignore-cert-errors', 'Ignore self signed certificate errors')
-    .option('--ca-cert <cert>', 'Specify a path for a cert used in SSL/TLS connection')
-    .option('--proxy <proxy>', 'Proxy URL to use for connections (optional). Can also use the environment variable "HTTPS_PROXY=[ip]:[port]" and "grcp_proxy=[ip]:[port]" for gRPC')
-    .option('--pac <pac>', 'Proxy auto configuration (optional). Specify a file, http url or ftp url')
+    .option(
+      '--ca-cert <cert>',
+      'Specify a path for a cert used in SSL/TLS connection'
+    )
+    .option(
+      '--proxy <proxy>',
+      'Proxy URL to use for connections (optional). Can also use the environment variable "HTTPS_PROXY=[ip]:[port]" and "grcp_proxy=[ip]:[port]" for gRPC'
+    )
+    .option(
+      '--pac <pac>',
+      'Proxy auto configuration (optional). Specify a file, http url or ftp url'
+    )
     .option('-v, --verbose', 'Makes scan operation verbose')
-    .action((source, options) => {scanHandler(source, options).catch((e) => {CLIErrorHandler(e)})})
-    .addHelpText('after', `
+    .action((source, options) => {
+      scanHandler(source, options).catch((e) => {
+        CLIErrorHandler(e);
+      });
+    })
+    .addHelpText(
+      'after',
+      `
   Examples:
     $ scanoss-js scan -o scan-output.json <source-folder>`
     );
 
-    program
+  program
     .command('dep <source>')
     .description('Scan for dependencies')
-    .option('-o, --output <filename>', 'Output result file name (optional - default stdout)')
-    .option('-a, --grpc-host <host>', 'SCANOSS GRPC HOST (optional - default: scanoss.com:443)')
-    .action((source, options) => {depHandler(source, options).catch((e) => {CLIErrorHandler(e)})})
+    .option(
+      '-o, --output <filename>',
+      'Output result file name (optional - default stdout)'
+    )
+    .option(
+      '-a, --grpc-host <host>',
+      'SCANOSS GRPC HOST (optional - default: scanoss.com:443)'
+    )
+    .action((source, options) => {
+      depHandler(source, options).catch((e) => {
+        CLIErrorHandler(e);
+      });
+    });
 
-    program
+  program
     .command('wfp <source>')
     .description('Generates fingerprints for a folder/file')
     .option('-H, --hpsm', 'Scan using winnowing high precision matching')
     .option('--obfuscate', 'Obfuscate fingerprints')
-    .option('-o, --output <filename>', 'Output result file name (optional - default stdout)')
-    .option('-p, --block-size <size>', 'Maximum size in Kb for each fingerprint block (optional - default 64Kb)')
-    .action((source, options) => {wfpHandler(source, options).catch((e) => {CLIErrorHandler(e)})})
+    .option(
+      '-o, --output <filename>',
+      'Output result file name (optional - default stdout)'
+    )
+    .option(
+      '-p, --block-size <size>',
+      'Maximum size in Kb for each fingerprint block (optional - default 64Kb)'
+    )
+    .action((source, options) => {
+      wfpHandler(source, options).catch((e) => {
+        CLIErrorHandler(e);
+      });
+    });
 
-    await program.parseAsync(process.argv);
+  await program.parseAsync(process.argv);
 }
-
 
 try {
   main();
