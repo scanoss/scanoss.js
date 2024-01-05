55a7e13d6b25a6fd917c0c75b8cdc41ba4397b32
Agustin Isasmendi <isasmendi.agus@gmail.com>
2024-01-05 15:45:50 +0100

    CLI-90 Replace testing files for JSON table

--- tests/sdk/Dependencies/LocalDependency/Parsers/samples/yarn-lock/generate_expected_output.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Command used to generate the expected output for yarn.lock with scancode
-scancode --json-pp - --package $1 | jq -c '.dependencies[] | { "purl": .purl , "requirement": .extracted_requirement | split(" ") | join(", ") }'
