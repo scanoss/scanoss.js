c0b5ce78030bc37834de15b11ec50fcc1bc8147b
Agustin Groh <agusgroh@gmail.com>
2025-04-08 14:12:14 -0300

    feat:SP-2328 Adds export control service

--- src/sdk/Services/Grpc/scanoss/api/cryptography/v2/scanoss-cryptography_grpc_pb.d.ts
+++ src/sdk/Services/Grpc/scanoss/api/cryptography/v2/scanoss-cryptography_grpc_pb.d.ts
@@ -10,6 +10,9 @@ import * as grpc from "@grpc/grpc-js";
 interface ICryptographyService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
   echo: grpc.MethodDefinition<scanoss_api_common_v2_scanoss_common_pb.EchoRequest, scanoss_api_common_v2_scanoss_common_pb.EchoResponse>;
   getAlgorithms: grpc.MethodDefinition<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmResponse>;
+  getAlgorithmsInRange: grpc.MethodDefinition<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmsInRangeResponse>;
+  getVersionsInRange: grpc.MethodDefinition<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.VersionsInRangeResponse>;
+  getHintsInRange: grpc.MethodDefinition<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.HintsInRangeResponse>;
 }
 
 export const CryptographyService: ICryptographyService;
@@ -17,6 +20,9 @@ export const CryptographyService: ICryptographyService;
 export interface ICryptographyServer extends grpc.UntypedServiceImplementation {
   echo: grpc.handleUnaryCall<scanoss_api_common_v2_scanoss_common_pb.EchoRequest, scanoss_api_common_v2_scanoss_common_pb.EchoResponse>;
   getAlgorithms: grpc.handleUnaryCall<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmResponse>;
+  getAlgorithmsInRange: grpc.handleUnaryCall<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmsInRangeResponse>;
+  getVersionsInRange: grpc.handleUnaryCall<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.VersionsInRangeResponse>;
+  getHintsInRange: grpc.handleUnaryCall<scanoss_api_common_v2_scanoss_common_pb.PurlRequest, scanoss_api_cryptography_v2_scanoss_cryptography_pb.HintsInRangeResponse>;
 }
 
 export class CryptographyClient extends grpc.Client {
@@ -27,4 +33,13 @@ export class CryptographyClient extends grpc.Client {
   getAlgorithms(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmResponse>): grpc.ClientUnaryCall;
   getAlgorithms(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmResponse>): grpc.ClientUnaryCall;
   getAlgorithms(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmResponse>): grpc.ClientUnaryCall;
+  getAlgorithmsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmsInRangeResponse>): grpc.ClientUnaryCall;
+  getAlgorithmsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmsInRangeResponse>): grpc.ClientUnaryCall;
+  getAlgorithmsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.AlgorithmsInRangeResponse>): grpc.ClientUnaryCall;
+  getVersionsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.VersionsInRangeResponse>): grpc.ClientUnaryCall;
+  getVersionsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.VersionsInRangeResponse>): grpc.ClientUnaryCall;
+  getVersionsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.VersionsInRangeResponse>): grpc.ClientUnaryCall;
+  getHintsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.HintsInRangeResponse>): grpc.ClientUnaryCall;
+  getHintsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.HintsInRangeResponse>): grpc.ClientUnaryCall;
+  getHintsInRange(argument: scanoss_api_common_v2_scanoss_common_pb.PurlRequest, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<scanoss_api_cryptography_v2_scanoss_cryptography_pb.HintsInRangeResponse>): grpc.ClientUnaryCall;
 }
