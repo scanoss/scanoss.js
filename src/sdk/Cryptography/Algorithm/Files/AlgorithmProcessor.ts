11c457abe078fda3ea89f4238e71876e46170848
Agustin Groh <agusgroh@gmail.com>
2025-04-25 07:41:06 -0300

    chore:SP-2391 Skips binary files and files larger than 2GB on local cryptography scanning

--- src/sdk/Cryptography/Algorithm/Files/AlgorithmProcessor.ts
+++ src/sdk/Cryptography/Algorithm/Files/AlgorithmProcessor.ts
@@ -4,16 +4,10 @@ const fs = require('fs');
 
 parentPort.on('message', async (job) => {
 
-    const MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024;
     const { data } = job;
     const { file, rules, cryptoMapper } = data;
 
      const cryptoFound = new Array();
-     const stats = await fs.promises.stat(file);
-     if (stats.size > MAX_FILE_SIZE) {
-      parentPort.postMessage({ file, algorithms:[] });
-      return;
-     }
 
       let content =  fs.readFileSync(file, 'utf-8');
        rules.forEach((value, key) => {
--- src/sdk/Cryptography/Hint/Files/HintProcessor.ts
+++ src/sdk/Cryptography/Hint/Files/HintProcessor.ts
@@ -4,15 +4,8 @@ const fs = require('fs');
 
 parentPort.on('message', async (job) => {
 
-    const MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024;
     const { data } = job;
     const { file, rules } = data;
-
-    const stats = await fs.promises.stat(file);
-    if (stats.size > MAX_FILE_SIZE) {
-       parentPort.postMessage({ file, hints: [] });
-       return;
-    }
 
     let content = fs.readFileSync(file, 'utf-8');
     const hints = [];
