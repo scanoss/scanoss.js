b27bcc08c7330dd71a77ddc0669aba802aa3a576
Agustin Groh <agusgroh@gmail.com>
2025-09-05 08:39:13 -0300

    chore:SP-3184 replace gRPC client by HTTP client on cryptography scanner

--- src/sdk/Clients/Transport/Transport.ts
+++ src/sdk/Clients/Transport/Transport.ts
@@ -2,12 +2,12 @@ import FormData from 'form-data';
 
 export abstract class Transport<T1> {
 
-  abstract get(url: string): Promise<T1>;
+  protected abstract get(url: string): Promise<T1>;
 
-  abstract post(url: string, body: FormData): Promise<T1>;
+  protected abstract post(url: string, body: FormData): Promise<T1>;
 
-  abstract put(url: string, body: FormData): Promise<T1>;
+  protected abstract put(url: string, body: FormData): Promise<T1>;
 
-  abstract delete(url: string): Promise<T1>;
+  protected abstract delete(url: string): Promise<T1>;
 
 }
